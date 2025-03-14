8302a368a6d70a364015b8bb4c1d8745bf9cb50b
thuongthuongthuong <huuthuongaa1@gmail.com>
2025-03-14 11:12:43 +0700

    Update web build with login screen

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3326764913"
+    serviceWorkerVersion: "3872324664"
   }
 });
